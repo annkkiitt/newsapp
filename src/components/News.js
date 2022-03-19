14373fad0a4744068800e7c443c4c9a2d99b266d
Ankit Rawat <ankit2001rawat@gmail.com>
2022-03-19 08:32:17 +0530

    video24

--- src/components/News.js
+++ src/components/News.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export default class News extends Component {
+  constructor(){
+    super();
+    console.log("Hello i'm from news components.")
+    this.state={
+      article: [],
+      loading: false
+    }
+  }
+
+//   life cycle method which will run after render
+    async componentDidMount(){
+    let URL = "https://newsapi.org/v2/top-headlines?country=in&apiKey=79c131ef12874f9591e7486d612c9016"
+    let data = await fetch(URL);
+    let parsedData = await data.json();
+    this.setState({article: parsedData.articles})
+  }
   render() {
+    
     return (
         <>
-         <div>This is newsComponent.</div>
-         <Newsitem/>
-         <Newsitem/>
-         <Newsitem/>
-         <Newsitem/>
+        <div className="container my-2">
+          <h1>NewsMonkey: Top news headlines of INDIA</h1>
+          <div className="row my-2">
+            {this.state.article.map((element)=>{
+                return <div className="col-md-4" key={element.url}>
+                  <Newsitem title={element.title?element.title:""} description={element.content?element.content.slice(0,100):""} imgUrl={element.urlToImage} newsUrl={element.url}/>
+                </div>
+            })}
+            
+          </div>
+        </div>
         </>
     )
   }
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
+  
   render() {
+    let {title,description,imgUrl,newsUrl} = this.props;
+
     return (
-      <div>This is Newsitem component.</div>
+      <>
+      <div className="card" style={{width: "18rem"}}>
+      <img src={imgUrl?imgUrl:"https://feeds.abplive.com/onecms/images/uploaded-images/2022/03/04/5d6215cd6a73953fbbc5ec2e2e7bb228_original.jpg?impolicy=abp_cdn&imwidth=1200&imheight=628"} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}...</p>
+        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
+      </div>
+    </div>
+      </>
     )
   }
 }
